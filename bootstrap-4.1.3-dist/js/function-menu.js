c3902f4e4fd5039a937add592e5afb90fd902960
YasminDalla <yasmindallarti837@gmail.com>
2020-07-29 12:35:01 -0300

    commit+projeto

---
+++ bootstrap-4.1.3-dist/js/function-menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#adiciona').click(function(e) {
+        e.preventDefault()
+        $('#conteudo').empty()
+        $('#conteudo').load('src/disciplinas/visao/adiciona-disciplinas.html')
+    })
+})
